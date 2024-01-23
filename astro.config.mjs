e08155ab85b1fb4fb08923da7cd5b3fdb00c748d
takuyaw-w <takuya.jamtb@gmail.com>
2024-01-24 03:35:37 +0900

    fix: 概ね完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import admonitions from "remark-github-beta-blockquote-admonitions";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +20,15 @@ export default defineConfig({
         twemoji: ["*"],
       },
     }),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
+  markdown: {
+    remarkPlugins: [admonitions],
+    rehypePlugins: [],
+  },
 });
