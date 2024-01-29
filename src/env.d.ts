6b8e55b0c70ba9c352c82a598d145a5cbb0c4fa1
takuyaw-w <takuya.jamtb@gmail.com>
2024-01-30 01:03:53 +0900

    fix: format適用

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-    readonly PUBLIC_SITE: string;
+  readonly PUBLIC_SITE: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv;
 }
